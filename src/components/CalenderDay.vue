7ece2d938ebd705588feb10cc24ee637454891eb
Lena Frohburg <s83854@informatik.htw-dresden.de>
2023-12-15 12:22:25 +0100

    test

---
+++ src/components/CalenderDay.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="body">
+      <div>
+        <p>Calendar</p>
+        <p style='font-weight:600'>Tasks</p>
+        <div class="task">
+          Einkaufen
+        </div>
+        
+        <div class="task">
+          Meeting
+        </div>
+  
+        <div class="task">
+          Post
+        </div>
+        
+        <div class="task">
+          Wäsche machen
+        </div>
+      </div>
+  
+      <div class="cal"><p>Overview</p></div>
+  
+    </div>
+     
+  </template>
